0b086b42f13cc1568fc61e91c83a2f60d4f1af0e
Tamta <tamtakopadz16@gmail.com>
2025-02-28 11:14:23 +0400

    set hosted API URL

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/', // Base URL for the local API server
+  apiUrl: 'https://recipe-space-api-production.up.railway.app/api/', // Base URL for the local API server
   imgbbApiKey: '2e2ab3cdcc924bfa4c98820d8fb261dd', // Active API key for uploading images to ImgBB
   // imgbbApiKey: 'a17c76d7bb1d953647a3575859a2e018',  // Use this key if the active API key encounters issues
 };
