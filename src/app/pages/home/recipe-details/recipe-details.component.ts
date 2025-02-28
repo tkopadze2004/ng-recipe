89f5d1f9901b00d36d172062a8df50b1db82be5e
Tamta <tamtakopadz16@gmail.com>
2025-02-28 20:11:31 +0400

    Implement loading spinner on recipe details page while fetching data

--- src/app/pages/home/recipe-details/recipe-details.component.ts
+++ src/app/pages/home/recipe-details/recipe-details.component.ts
@@ -6,16 +6,26 @@ import {
 } from '@angular/core';
 import { RecipeService } from '../../../services/recipe.service';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-import { Observable, Subject, switchMap, takeUntil } from 'rxjs';
+import {
+  catchError,
+  finalize,
+  Observable,
+  of,
+  Subject,
+  switchMap,
+  takeUntil,
+  tap,
+} from 'rxjs';
 import { IRecipe } from '../../../core/interfaces/recipe.interface';
 import { AsyncPipe } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-recipe-details',
   standalone: true,
-  imports: [AsyncPipe, RouterLink, MatButtonModule],
+  imports: [AsyncPipe, RouterLink, MatButtonModule, MatProgressSpinnerModule],
   templateUrl: './recipe-details.component.html',
   styleUrl: './recipe-details.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -26,12 +36,22 @@ export class RecipeDetailsComponent implements OnDestroy {
   private router: Router = inject(Router);
   private snackBar: MatSnackBar = inject(MatSnackBar);
   private readonly sub$ = new Subject();
+  public isLoading: boolean = true;
 
-  //Fetches the recipe details based on the recipe ID from the URL parameters.
-  public recipe$: Observable<IRecipe> = this.activatedroute.params.pipe(
-    switchMap((params) => this.recipeService.getRecipeById(params['id']))
+  // Fetches the recipe details based on the recipe ID from the URL parameters.
+  public recipe$: Observable<IRecipe | null> = this.activatedroute.params.pipe(
+    switchMap((params) => this.recipeService.getRecipeById(params['id'])),
+    tap(() => {
+      this.isLoading = false;
+    }),
+    catchError((error) => {
+      this.isLoading = false;
+      return of(null);
+    }),
+    finalize(() => {
+      this.isLoading = false;
+    })
   );
-
   /*
    * Delete  recipe by ID.
    * After deletion, displays a confirmation message and redirects to the home page.
